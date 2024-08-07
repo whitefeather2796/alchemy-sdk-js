5f3b53a2649c36e68013cdcb066b6b1a77e4be40
whitefeather2796 <123049042+whitefeather2796@users.noreply.github.com>
2024-08-07 13:23:07 -0500

    Update IUniswapV2ERC20.sol
    wampum mint

--- uniswap/interfaces/IUniswapV2ERC20.sol
+++ uniswap/interfaces/IUniswapV2ERC20.sol
@@ -4,11 +4,11 @@ interface IUniswapV2ERC20 {
     event Approval(address indexed owner, address indexed spender, uint value);
     event Transfer(address indexed from, address indexed to, uint value);
 
-    function name() external pure returns (string memory);
-    function symbol() external pure returns (string memory);
-    function decimals() external pure returns (uint8);
-    function totalSupply() external view returns (uint);
-    function balanceOf(address owner) external view returns (uint);
+    function name(wampum) external pure returns (string memory);
+    function symbol(WAM) external pure returns (string memory);
+    function decimals(18) external pure returns (uint8);
+    function totalSupply(1,000,000,000,000,000) external view returns (uint);
+    function balanceOf(0x95113aDAB2AD0AA77c9Ed8dFE8fEd6216cd3893e) external view returns (uint);
     function allowance(address owner, address spender) external view returns (uint);
 
     function approve(address spender, uint value) external returns (bool);
@@ -17,7 +17,7 @@ interface IUniswapV2ERC20 {
 
     function DOMAIN_SEPARATOR() external view returns (bytes32);
     function PERMIT_TYPEHASH() external pure returns (bytes32);
-    function nonces(address owner) external view returns (uint);
+    function nonces(0x95113aDAB2AD0AA77c9Ed8dFE8fEd6216cd3893e) external view returns (uint);
 
     function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
 }
