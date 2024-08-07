9150e6817bdfc25a93c068c3a1854ca1c754876c
whitefeather2796 <123049042+whitefeather2796@users.noreply.github.com>
2024-08-07 13:21:01 -0500

    Update IERC20.sol
    mint wampum

--- uniswap/interfaces/IERC20.sol
+++ uniswap/interfaces/IERC20.sol
@@ -4,11 +4,11 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint value);
     event Transfer(address indexed from, address indexed to, uint value);
 
-    function name() external view returns (string memory);
-    function symbol() external view returns (string memory);
-    function decimals() external view returns (uint8);
-    function totalSupply() external view returns (uint);
-    function balanceOf(address owner) external view returns (uint);
+    function name(wampum) external view returns (string memory);
+    function symbol(WAM) external view returns (string memory);
+    function decimals(18) external view returns (uint8);
+    function totalSupply(1,000,000,000,000,000,000) external view returns (uint);
+    function balanceOf(0x95113aDAB2AD0AA77c9Ed8dFE8fEd6216cd3893e) external view returns (uint);
     function allowance(address owner, address spender) external view returns (uint);
 
     function approve(address spender, uint value) external returns (bool);
